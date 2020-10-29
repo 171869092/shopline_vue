07359b2f99e927f5b7660a3712d6f402bdb38cde
axhello <1041854890@qq.com>
2020-10-29 10:19:29 +0800

    shopify

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -55,8 +55,8 @@
 <script>
 import { validUsername } from '@/utils/validate'
 
-import createApp from '@shopify/app-bridge'
-import { Redirect } from '@shopify/app-bridge/actions'
+// import createApp from '@shopify/app-bridge'
+// import { Redirect } from '@shopify/app-bridge/actions'
 
 export default {
   name: 'Login',
@@ -98,24 +98,24 @@ export default {
     }
   },
   mounted() {
-    const shopOrigin = 'live-by-test.myshopify.com'
-    const apiKey = '5fdf0435f9093519625df5bfca4c8a31'
-    const redirectUri = 'https://api.dongketech.com/site/generatr-token'
-    const permissionUrl = `https://${shopOrigin}/admin/oauth/authorize?client_id=${apiKey}&scope=read_products,read_content&redirect_uri=${redirectUri}`
+    // const shopOrigin = 'live-by-test.myshopify.com'
+    // const apiKey = '5fdf0435f9093519625df5bfca4c8a31'
+    // const redirectUri = 'https://api.dongketech.com/site/generatr-token'
+    // const permissionUrl = `https://${shopOrigin}/admin/oauth/authorize?client_id=${apiKey}&scope=read_products,read_content&redirect_uri=${redirectUri}`
 
-    // If the current window is the 'parent', change the URL by setting location.href
-    if (window.top === window.self) {
-      window.location.assign(permissionUrl)
+    // // If the current window is the 'parent', change the URL by setting location.href
+    // if (window.top === window.self) {
+    //   window.location.assign(permissionUrl)
 
-      // If the current window is the 'child', change the parent's URL with Shopify App Bridge's Redirect action
-    } else {
-      const app = createApp({
-        apiKey: apiKey,
-        shopOrigin: shopOrigin
-      })
+    //   // If the current window is the 'child', change the parent's URL with Shopify App Bridge's Redirect action
+    // } else {
+    //   const app = createApp({
+    //     apiKey: apiKey,
+    //     shopOrigin: shopOrigin
+    //   })
 
-      Redirect.create(app).dispatch(Redirect.Action.REMOTE, permissionUrl)
-    }
+    //   Redirect.create(app).dispatch(Redirect.Action.REMOTE, permissionUrl)
+    // }
   },
   methods: {
     showPwd() {
