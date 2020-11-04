29d6891939bb3f611f2d9b63f67b17b9ec4fbdc2
liu_bin <1464895828@qq.com>
2020-11-04 17:43:45 +0800

    ces

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
-// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('../mock')
   mockXHR()
 }
-// Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale })
 
 Vue.use(FBSignInButton)
 // 如果想要中文版 element-ui，按如下方式声明
