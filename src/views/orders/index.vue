7d72e14dbcd7d8808cadea9cfdb7b91deca6f761
axhello <1041854890@qq.com>
2020-12-04 14:31:41 +0800

    feat:优化

--- src/views/orders/index.vue
+++ src/views/orders/index.vue
@@ -13,11 +13,38 @@
             :header-cell-style="{background:'#F3F5F9FF',color:'#262B3EFF'}"
           >
             <!-- <el-table-column type="selection" /> -->
+            <el-table-column label="Expand" type="expand" width="100">
+              <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                  <el-form-item label="商品名称">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="所属店铺">
+                    <span>{{ props.row.shop }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品 ID">
+                    <span>{{ props.row.id }}</span>
+                  </el-form-item>
+                  <el-form-item label="店铺 ID">
+                    <span>{{ props.row.shopId }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品分类">
+                    <span>{{ props.row.category }}</span>
+                  </el-form-item>
+                  <el-form-item label="店铺地址">
+                    <span>{{ props.row.address }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品描述">
+                    <span>{{ props.row.desc }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
+            </el-table-column>
             <el-table-column v-for="(item,idx) in labelList" :key="idx" :label="item.label" :prop="item.value" :width="item.width">
               <template slot-scope="scope">
                 <span v-if="item.type == undefined">{{ scope.row[item.value] }}</span>
                 <span v-if="item.type == 'order_no'">
-                  <span class="link" @click="toLink(scope.row)">{{ scope.row.order_no }}</span>
+                  <span class="link" @click="toLink(scope.row)">{{ scope.row.thirdParty_order_on }}</span>
                 </span>
                 <span v-if="item.type == 'order_status'">
                   <div>{{ orderStatus[scope.row.order_status] }}</div>
@@ -78,8 +105,8 @@ export default {
         { label: 'Return Goods In Stock', name: '9' }
       ],
       labelList: [
-        { label: 'Order Number', value: 'order_no', type: 'order_no' },
-        { label: 'Third Party Order Number', value: 'thirdParty_order_on' },
+        // { label: 'Order Number', value: 'order_no', type: 'order_no' },
+        { label: 'Third Party Order Number', value: 'thirdParty_order_on', type: 'order_no' },
         { label: 'Order Amount', value: 'total_price' },
         { label: 'Order Status', value: 'order_status_name', type: 'order_status' },
         { label: 'Logistics Status', value: 'logistics_status_name', type: 'logistics_status' }
