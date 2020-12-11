332d4c478c9711e4186b222137c477438392499a
axhello <1041854890@qq.com>
2020-12-11 11:28:31 +0800

    feat:dashboard页面优化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">Dashboard</div>
+  <div class="dashboard-editor-container">
+    <!-- <github-corner class="github-corner" /> -->
+
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+
+    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData" />
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <pie-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart />
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+import RaddarChart from './components/RaddarChart'
+import PieChart from './components/PieChart'
+import BarChart from './components/BarChart'
 import { mapGetters } from 'vuex'
 import { shopifyPush } from '@/api/user'
 // import createApp from '@shopify/app-bridge'
 // import { Redirect, Button } from '@shopify/app-bridge/actions'
 // import { getSessionToken } from '@shopify/app-bridge-utils'
 import { getStorage } from '@/utils/storage'
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
 export default {
   name: 'dashboard',
+  components: {
+    PanelGroup,
+    LineChart,
+    RaddarChart,
+    PieChart,
+    BarChart
+  },
+  data() {
+    return {
+      lineChartData: lineChartData.newVisitis
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
@@ -59,6 +120,9 @@ export default {
           })
         }, 300000)
       }
+    },
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
     }
   }
 }
@@ -72,6 +136,13 @@ export default {
   &-text {
     font-size: 30px;
     line-height: 46px;
+  }
+}
+.dashboard-editor-container {
+  padding: 30px;
+  .chart-wrapper {
+    background: #fff;
+    padding: 20px 20px;
   }
 }
 </style>
