a98426361e18d811d99b40dcc55cf87de243c85a
axhello <1041854890@qq.com>
2020-12-11 11:29:14 +0800

    feat:dashboard页面优化

--- src/permission.js
+++ src/permission.js
@@ -8,7 +8,7 @@ import { getToken } from '@/utils/auth' // get token from cookie
 // import { setToken } from '@/utils/auth'
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-const whiteList = ['/login', '/register','/'] // no redirect whitelist
+const whiteList = ['/login', '/register', '/'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
   const hasToken = getToken()
