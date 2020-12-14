93e368aae1eebc7466e1492baa2f594efc6692fd
axhello <1041854890@qq.com>
2020-12-14 14:28:27 +0800

    feat:订单接口更改

--- src/api/orders.js
+++ src/api/orders.js
@@ -7,23 +7,34 @@ export function getOrderTabs(params) {
     params
   })
 }
+// 订单列表
 export function getOrderList(params) {
   return request({
-    url: '/order/order/index',
+    url: '/order_index',
     method: 'get',
     params
   })
 }
+// 订单物流
+export function getLogisticIndex(params) {
+  return request({
+    url: '/order_logistics_index',
+    method: 'get',
+    params
+  })
+}
+// 订单物流
 export function getLogisticInfo(params) {
   return request({
-    url: '/order/order-logistics/way-bill-info',
+    url: '/order_logistics_info',
     method: 'get',
     params
   })
 }
+// 订单详情
 export function getOrderInfo(params) {
   return request({
-    url: '/order/order/get-order-info',
+    url: '/order_info',
     method: 'get',
     params
   })
