7620a12c62d89dec5ec8053b85cdcf9fa686309a
axhello <zhanglin-pc@live.com>
2021-01-26 10:31:17 +0800

    fix:shopify店铺链接

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-const plugins = ['@vue/babel-plugin-transform-vue-jsx']
-// 生产环境移除console
-if (process.env.NODE_ENV === 'production') {
-  plugins.push('transform-remove-console')
-}
+// const plugins = ['@vue/babel-plugin-transform-vue-jsx']
+// // 生产环境移除console
+// if (process.env.NODE_ENV === 'production') {
+//   plugins.push('transform-remove-console')
+// }
 module.exports = {
-  plugins: plugins,
+  // plugins: plugins,
   presets: [
     // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
     '@vue/cli-plugin-babel/preset'
