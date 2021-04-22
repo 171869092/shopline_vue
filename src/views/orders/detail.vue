ce16d583993cf67bcce4a503f3067ca2c8f54171
breeze <yudingzuo0817@163.com>
2021-04-22 19:39:20 +0800

    Re-render order detail

--- src/views/orders/detail.vue
+++ src/views/orders/detail.vue
@@ -12,127 +12,139 @@
           <div class="order-id ml20">
             Order No：<span class="primary">{{ order_no }}</span>
           </div>
+          <el-select v-model="selectedProduct" v-if="isShowProductIndex" @change="handleChange(selectedProduct)" placeholder="Select Product">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
         </div>
-        <el-card class="box-card mt20">
-          <div slot="header">
-            <div class="detail-block-title">
-              <h2>Product</h2>
-            </div>
-          </div>
-          <el-table
-            ref="productTable"
-            fit
-            :data="detailInfo.goods_info"
-            style="width: 100%"
-            highlight-current-row
-            :header-cell-style="{ background: '#F3F5F9FF', color: '#262B3EFF' }"
-          >
-            <el-table-column label="Picture" width="150">
-              <template slot-scope="scope">
-                <el-image class="sku_image" :src="scope.row.sku_image" fit="cover">
-                  <div slot="error" class="image-slot">
-                    <i class="error-icon el-icon-picture-outline" />
-                  </div>
-                </el-image>
-              </template>
-            </el-table-column>
-            <el-table-column prop="third_goods_name" label="Products" />
-            <el-table-column prop="third_sku_name" label="Price&Amount" width="200">
-              <template slot-scope="scope">
-                <span>{{ `${scope.row.third_price} x ${scope.row.sku_num}` }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column prop="total_price" label="Total Price" width="200" />
-            <el-table-column prop="service_name" label="Vendor" width="200">
-              <template slot-scope="scope">
-                <span>{{ scope.row.service_name || '--' }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column prop="purchase_price" label="Total Cost" width="200">
-              <template slot-scope="scope">
-                <span>{{ scope.row.purchase_price || '--' }}</span>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-card>
-
-        <el-card class="box-card mt20">
-          <div slot="header">
-            <div class="detail-block-title">
-              <h2>Customer</h2>
-            </div>
-          </div>
-          <el-row>
-            <el-col :offset="1" :span="8">
-              <div class="flexbox">
-                <div class="vendor-sub-title"><span>Shipping address</span></div>
-              </div>
-              <ul class="customer-info mt15">
-                <li><span>Name: </span><span>{{ detailInfo.consignee }}</span></li>
-                <li><span>Street: </span><span>{{ detailInfo.address1 }}</span></li>
-                <li><span>Zipcode: </span><span>{{ detailInfo.zip }}</span></li>
-                <li><span>Country: </span><span>{{ detailInfo.country }}</span></li>
-                <li><span>Phone: </span><span>{{ detailInfo.mobile }}</span></li>
-              </ul>
-            </el-col>
-            <el-col :span="10">
-              <div class="flexbox">
-                <div class="vendor-sub-title"><span>Contact information</span></div>
-              </div>
-              <ul class="customer-info mt15">
-                <li><span>Email: </span><span>{{ detailInfo.email }}</span></li>
-              </ul>
-            </el-col>
-          </el-row>
-        </el-card>
-
-        <el-card class="box-card mt20">
-          <div slot="header">
-            <div class="detail-block-title">
-              <h2>Vendor</h2>
-            </div>
-          </div>
-          <div v-for="(vendor, key) in vendors" :key="key" class="border-top">
-            <el-row :gutter="20">
-              <el-col :offset="1" :span="8">
-                <div class="grid-content bg-purple">
-                  <div class="flexbox">
-                    <div class="vendor-sub-title">Name: <span>{{ vendor.service_name || '--' }}</span></div>
-                  </div>
-                  <ul class="vendor-info mt15">
-                    <li><span>Total Cost: </span><span>${{ vendor.total_cost }}</span></li>
-                    <li><span>Product Price: </span><span>${{ vendor.product_price }}</span></li>
-                    <li><span>Shipping Price: </span><span>${{ vendor.shipping_price }}</span></li>
-                    <li><span>Service Price: </span><span>${{ vendor.service_price }}</span></li>
-                  </ul>
+        <div v-for="(info,key) in detailInfo" :key="key" class="order-cell" :id="gernerateId(key)">
+          <el-card class="box-card mt20">
+            <div slot="header">
+              <div class="detail-block-title">
+                <div >
+                  <h2>Product <span v-if="isShowProductIndex">{{ key+1 }}</span></h2>
                 </div>
-              </el-col>
-              <el-col :span="15">
-                <div class="grid-content bg-purple">
-                  <div class="flexbox">
-                    <div class="vendor-sub-title">Status: <span>{{ vendor.track_info.TrackingStatus || '--' }}</span></div>
-                    <div class="vendor-sub-title ml40">Tracking Numbers: <span>{{ vendor.track_info.WaybillNumber || '--' }}</span></div>
-                  </div>
-                  <el-timeline v-if=" vendor.track_info.OrderTrackingDetails" class="mt20 pl20" :reverse="false">
-                    <el-timeline-item
-                      v-for="(item, index) in vendor.track_info.OrderTrackingDetails"
-                      :key="index"
-                      type="primary"
-                    >
-                      <span>{{ item.ProcessDate }}</span><span class="ml40">{{ item.ProcessContent }}</span>
-                    </el-timeline-item>
-                  </el-timeline>
-                  <div v-else class="empty-text ml40 mt40">
-                    <div class="empty-icon">
-                      <svg-icon icon-class="nodata" />
+              </div>
+            </div>
+            <el-table
+              ref="productTable"
+              fit
+              :data="info.goods_info"
+              style="width: 100%"
+              highlight-current-row
+              :header-cell-style="{ background: '#F3F5F9FF', color: '#262B3EFF' }"
+            >
+              <el-table-column label="Picture" width="150">
+                <template slot-scope="scope">
+                  <el-image class="sku_image" :src="scope.row.sku_image" fit="cover">
+                    <div slot="error" class="image-slot">
+                      <i class="error-icon el-icon-picture-outline" />
                     </div>
-                    <div class="empty-normal">No Track Info</div>
-                  </div>
+                  </el-image>
+                </template>
+              </el-table-column>
+              <el-table-column prop="third_goods_name" label="Products" width="150"/>
+              <el-table-column prop="third_sku_name" label="Price&Amount" width="200">
+                <template slot-scope="scope">
+                  <span>{{ `${scope.row.third_price} x ${scope.row.sku_num}` }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="total_price" label="Total Price" width="200" />
+              <el-table-column prop="service_name" label="Vendor" width="200">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.service_name || '--' }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="purchase_price" label="Total Cost" width="200">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.purchase_price || '--' }}</span>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-card>
+
+          <el-card class="box-card mt20">
+            <div slot="header">
+              <div class="detail-block-title">
+                <h2>Customer</h2>
+              </div>
+            </div>
+            <el-row>
+              <el-col :offset="1" :span="8">
+                <div class="flexbox">
+                  <div class="vendor-sub-title"><span>Shipping address</span></div>
                 </div>
+                <ul class="customer-info mt15">
+                  <li><span>Name: </span><span>{{ info.consignee }}</span></li>
+                  <li><span>Street: </span><span>{{ info.address1 }}</span></li>
+                  <li><span>Zipcode: </span><span>{{ info.zip }}</span></li>
+                  <li><span>Country: </span><span>{{ info.country }}</span></li>
+                  <li><span>Phone: </span><span>{{ info.mobile }}</span></li>
+                </ul>
+              </el-col>
+              <el-col :span="10">
+                <div class="flexbox">
+                  <div class="vendor-sub-title"><span>Contact information</span></div>
+                </div>
+                <ul class="customer-info mt15">
+                  <li><span>Email: </span><span>{{ info.email }}</span></li>
+                </ul>
               </el-col>
             </el-row>
-          </div>
-        </el-card>
+          </el-card>
+
+          <el-card class="box-card mt20">
+            <div slot="header">
+              <div class="detail-block-title">
+                <h2>Vendor</h2>
+              </div>
+            </div>
+            <div class="border-top">
+              <el-row :gutter="20">
+                <el-col :offset="1" :span="8">
+                  <div class="grid-content bg-purple">
+                    <div class="flexbox">
+                      <div class="vendor-sub-title">Name: <span>{{ info.vendor.service_name || '--' }}</span></div>
+                    </div>
+                    <ul class="vendor-info mt15">
+                      <li><span>Total Cost: </span><span>${{ info.vendor.total_cost }}</span></li>
+                      <li><span>Product Price: </span><span>${{ info.vendor.product_price }}</span></li>
+                      <li><span>Shipping Price: </span><span>${{ info.vendor.shipping_price }}</span></li>
+                      <li><span>Service Price: </span><span>${{ info.vendor.service_price }}</span></li>
+                    </ul>
+                  </div>
+                </el-col>
+                <el-col :span="15">
+                  <div class="grid-content bg-purple">
+                    <div class="flexbox">
+                      <div class="vendor-sub-title">Status: <span>{{ info.vendor.track_info.TrackingStatus || '--' }}</span></div>
+                      <div class="vendor-sub-title ml40">Tracking Numbers: <span>{{ info.vendor.track_info.WaybillNumber || '--' }}</span></div>
+                    </div>
+                    <el-timeline v-if=" info.vendor.track_info.OrderTrackingDetails" class="mt20 pl20" :reverse="false">
+                      <el-timeline-item
+                        v-for="(item, index) in info.vendor.track_info.OrderTrackingDetails"
+                        :key="index"
+                        type="primary"
+                      >
+                        <span>{{ item.ProcessDate }}</span><span class="ml20">{{ item.ProcessContent }}</span>
+                      </el-timeline-item>
+                    </el-timeline>
+                    <div v-else class="empty-text ml40 mt40">
+                      <div class="empty-icon">
+                        <svg-icon icon-class="nodata" />
+                      </div>
+                      <div class="empty-normal">No Track Info</div>
+                    </div>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </el-card>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -153,9 +165,8 @@ export default {
         { label: 'Service Fee', value: 'service_fee' }
       ],
       detailInfo: {},
-      vendors: [],
       loading: false,
-      trackList: []
+      selectedProduct: '',
     }
   },
   computed: {
@@ -164,9 +175,25 @@ export default {
     },
     order_no() {
       return this.$route.query.order_no
+    },
+    isShowProductIndex() {
+      return this.detailInfo.length > 1
+    },
+    options () {
+      let len = this.detailInfo.length
+      let arr = []
+      if (len < 2) {
+        return []
+      } else {
+        for (let i=0; i<len; i++) {
+          arr.push({label:'Product'+(i+1), value:i})
+        }
+      }
+      return arr
     }
   },
   created() {
+    document.getElementsByTagName("body")[0].className="bd";
     this.init()
   },
   methods: {
@@ -180,11 +207,9 @@ export default {
       this.loading = true
       getOrderInfo({ orders_id: this.order_id })
         .then((res) => {
-          // console.log(res.data)
+          console.log(res.data)
           if (res.code === 200) {
             this.detailInfo = res.data
-            this.vendors = res.data.vendor
-            console.log(this.vendors)
           }
         })
         .catch((err) => {
@@ -194,14 +219,39 @@ export default {
           // loading.close()
           this.loading = false
         })
-    }
+    },
+    gernerateId(index) {
+      return 'product'+index
+    },
+    handleChange(index) {
+      let id = 'product'+index;
+      document.getElementById(id).scrollIntoView();
+      // this.$nextTick(() => {
+      //   setTimeout(() => {
+      //       let targetbox= document.getElementById(id);
+      //       this.target= targetbox.offsetTop;
+      //   })
+      // })
+      // document.body.scrollTop = this.target;
+    },
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.bd {
+  overflow: auto;
+}
+.order-id {
+  line-height: 36px;
+  margin-right: 20px;
+}
 .order-detail {
+  overflow: auto;
   .order-header {
     line-height: 30px;
+  }
+  .order-cell {
+    overflow: auto;
   }
   .button-border {
     border: 1px solid #ef6f38;
