50007b31fb87184fad81dfd086489cf6cbf37f2c
lijunchen <171869092@qq.com>
2021-04-27 19:12:20 +0800

    space fix bug

--- src/views/settings/components/password.vue
+++ src/views/settings/components/password.vue
@@ -2,28 +2,26 @@
     <div>22222</div>
 </template>
 <script>
-    export default {
-        name: 'password',
-        components: {
-            
+export default {
+    name: 'password',
+    components: {},
+    props : {},
+    data () {
+        return {
+        formQuery: {},
+        listQuery: {
+            total: 0,
+            page: 1,
+            limit: 10
         },
-        props : {},
-        data () {
-            return {
-            formQuery: {},
-            listQuery: {
-                total: 0,
-                page: 1,
-                limit: 10
-            },
-                tableData: [],
-                loading: false
-            }
-        },
-        computed: {},
-        created() {},
-        methods: {}
-    }
+            tableData: [],
+            loading: false
+        }
+    },
+    computed: {},
+    created() {},
+    methods: {}
+}
 </script>
 <style scoped>
 </style>
