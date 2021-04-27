f0cf9d87a2120e2581842b42294ef1f7802d1bc2
liu_bin <1464895828@qq.com>
2021-04-27 19:13:44 +0800

    格式对齐--何晓凤

--- src/views/settings/components/password.vue
+++ src/views/settings/components/password.vue
@@ -1,26 +1,26 @@
 <template>
-    <div>22222</div>
+  <div>22222</div>
 </template>
 <script>
 export default {
-    name: 'password',
-    components: {},
-    props : {},
-    data () {
-        return {
-        formQuery: {},
-        listQuery: {
-            total: 0,
-            page: 1,
-            limit: 10
-        },
-            tableData: [],
-            loading: false
-        }
-    },
-    computed: {},
-    created() {},
-    methods: {}
+  name: 'password',
+  components: {},
+  props: {},
+  data() {
+    return {
+      formQuery: {},
+      listQuery: {
+        total: 0,
+        page: 1,
+        limit: 10
+      },
+      tableData: [],
+      loading: false
+    }
+  },
+  computed: {},
+  created() {},
+  methods: {}
 }
 </script>
 <style scoped>
